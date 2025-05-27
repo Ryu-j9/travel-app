e9d962c6e10403c5f4bb34d3c3d618d5a358e4ca
Ryu <fug2357@naver.com>
2025-05-27 13:03:36 +0900

    Initial commit

---
+++ screens/FavoritesScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const FavoritesScreen = () => {
+    return (
+        <View>
+            <Text>찜한 관광지 페이지</Text>
+        </View>
+    )
+}
+
+export default FavoritesScreen
